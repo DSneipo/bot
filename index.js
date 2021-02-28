0fd96178a69f346c170c3c11863614e3983415e5
Sneipo <79805028+DSneipo@users.noreply.github.com>
2021-03-01 01:40:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const_TelegramBot=require ('node-telegram-bot-api')
 const os = require ('os')
 
-const bot = new TelegramBot ('1635277664:AAHTRqgf5b31ztG-KtV9dhmTlL748EXWtoE', {polling: tru})
+const bot = new TelegramBot ('1635277664:AAHTRqgf5b31ztG-KtV9dhmTlL748EXWtoE', {polling: true})
 bot.on('message', msg => {
 bot.sendMessage(msg.chat.id, 'Hello from ${os.type()}, bot says hi to"${msg.from.first_name}"')
 })
